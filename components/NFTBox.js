4f3ac1fb83ebbb3613c63c2d3849902d635d6818
Valodax <snitzel098@gmail.com>
2022-06-13 18:16:07 +0200

    build test

--- components/NFTBox.js
+++ components/NFTBox.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { useWeb3Contract, useMoralis } from "react-moralis"
 import nftMarketplaceAbi from "../constants/NftMarketplace.json"
 import nftAbi from "../constants/BasicNft.json"
+import Image from "next/image"
 import { Card, useNotification } from "web3uikit"
 import { ethers } from "ethers"
 import UpdateListingModal from "./UpdateListingModal"
@@ -124,9 +125,15 @@ export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress,
                                 <div className="flex flex-col items-end gap-2">
                                     <div className="text-base">TokenID: {tokenId}</div>
                                     <div className="italic text-sm">
-                                        Current Owner: {formattedSellerAddress}
+                                        Owner: {formattedSellerAddress}
                                     </div>
-                                    <img src={imageURI} width="200" height="200" />
+                                    <Image
+                                        loader={() => imageURI}
+                                        src={imageURI}
+                                        objectFit="contain"
+                                        width="200"
+                                        height="200"
+                                    />
                                     <div className="font-bold">
                                         {isNaN(price)
                                             ? "Sold"
